ccb9ce67e927b73027699baa9b2d34c8f40d5ce0
umgn <91313067+umgn@users.noreply.github.com>
2023-03-09 18:22:39 +0900

    feat : 회사문서 조회 기능 추가
    author : seonhwakoo
    
    회사문서 리스트 조회 기능 추가
    
    - DocumentController.java
    - DocumentService.java
    - DocumentServiceImpl.java
    - DocumnetDao.java
    - document-mapper.xml
    - commonDocs.jsp

--- src/main/java/com/ppicachu/ppic/document/model/dao/DocumentDao.java
+++ src/main/java/com/ppicachu/ppic/document/model/dao/DocumentDao.java
@@ -1,8 +1,26 @@
 package com.ppicachu.ppic.document.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.ppicachu.ppic.common.model.vo.PageInfo;
+import com.ppicachu.ppic.document.model.vo.Document;
 
 @Repository
 public class DocumentDao {
 
+	public int selectCommonDocsCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("documentMapper.selectCommonDocsCount");
+	}
+	
+	public ArrayList<Document> selectCommonDocs(SqlSessionTemplate sqlSession, PageInfo pi){
+		int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit(); 
+		int limit = pi.getBoardLimit();
+				
+		RowBounds rbounds = new RowBounds(offset, limit);
+		return (ArrayList)sqlSession.selectList("documentMapper.selectCommonDocs", null, rbounds);
+	}
 }
--- src/main/java/com/ppicachu/ppic/document/model/service/DocumentService.java
+++ src/main/java/com/ppicachu/ppic/document/model/service/DocumentService.java
@@ -1,5 +1,14 @@
 package com.ppicachu.ppic.document.model.service;
 
+import java.util.ArrayList;
+
+import com.ppicachu.ppic.common.model.vo.PageInfo;
+import com.ppicachu.ppic.document.model.vo.Document;
+
 public interface DocumentService {
 
+	// 회사문서 리스트 조회
+	int selectCommonDocsCount();
+	ArrayList<Document> selectCommonDocs(PageInfo pi);
+	
 }
